31d960c3290acdf9ba05eb977cdde5a781c57fb8
rachelhox <rachelhoyuenyi@gmail.com>
2021-12-30 17:05:31 +0800

    social media update

--- src/components/social_media/utils.tsx
+++ src/components/social_media/utils.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { LinkedinIcon, TwitterIcon, GithubIcon, MediumIcon } from "@icons";
+import {
+  EmailIcon,
+  LinkedinIcon,
+  TwitterIcon,
+  GithubIcon,
+  MediumIcon,
+} from "@icons";
 
 export const socialMediaLinks: {
   component: React.ReactNode;
@@ -7,23 +13,18 @@ export const socialMediaLinks: {
   url: string;
 }[] = [
   {
-    component: <LinkedinIcon />,
-    className: "linkedin",
-    url: "https://discord.desmos.network",
+    component: <EmailIcon />,
+    className: "email",
+    url: "mailto:info@forbole.com",
   },
   {
-    component: <GithubIcon />,
-    className: "github",
-    url: "https://github.com/desmos-labs",
+    component: <LinkedinIcon />,
+    className: "linkedin",
+    url: "https://www.linkedin.com/company/forbole-ventures/",
   },
   {
     component: <TwitterIcon />,
     className: "twitter",
-    url: "https://twitter.com/DesmosNetwork",
-  },
-  {
-    component: <MediumIcon />,
-    className: "medium",
-    url: "https://medium.com/desmosnetwork",
+    url: "https://twitter.com/ForboleV",
   },
 ];
